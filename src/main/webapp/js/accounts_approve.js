51f975902299348c3f203a911eeeb826bea8fa45
lekingzareef <zareefsoeto@gmail.com>
2021-04-25 23:20:07 -0300

    Read Loans Done.

--- src/main/webapp/js/accounts_approve.js
+++ src/main/webapp/js/accounts_approve.js
@@ -12,25 +12,28 @@ function getAccountsList() {
                 accounts.forEach(item => {
                     console.log(item);
                     var date = new Date(item.date_open);
-                    const html =
-                        `
-                            <tr>
-                                <td>${item.id}</td>
-                                <td>${item.currency}</td>
-                                <td>${item.accountType.id}</td>
-                                <td>${item.accountType.occupation}</td>
-                                <td>${item.amount}</td>
-                                <td>${item.yield}</td>
-                                <td>${item.customer.name}</td>
-                                <td>${date.getDate()+
-                                    "/"+(date.getMonth()+1)+
-                                    "/"+date.getFullYear()}</td>
-                                <td>
-                                    <button class="btn btn-danger" onclick="alert(${item.id})">Approve</button>
-                                </td>
-                            </tr>
-                        `;
-                    document.getElementById("accountList").innerHTML += html;
+                    
+                    if (item.status == "open") { 
+                        const html =
+                            `
+                                <tr>
+                                    <td>${item.id}</td>
+                                    <td>${item.currency}</td>
+                                    <td>${item.accountType.id}</td>
+                                    <td>${item.accountType.occupation}</td>
+                                    <td>${item.amount}</td>
+                                    <td>${item.yield}</td>
+                                    <td>${item.customer.name}</td>
+                                    <td>${date.getDate()+
+                                        "/"+(date.getMonth()+1)+
+                                        "/"+date.getFullYear()}</td>
+                                    <td>
+                                       <button class="btn btn-success" onclick="alert(${item.id})">Approve</button>
+                                    </td>
+                                </tr>
+                            `;
+                        document.getElementById("accountList").innerHTML += html;
+                    }
                 });
             } else {
                 alert("Something went wrong retreiving the data");
